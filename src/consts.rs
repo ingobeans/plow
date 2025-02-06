e0daa5575de73ae0648a9b509f4e3c2b4418972d
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-02-06 12:45:31 +0100

    initial commit

---
+++ src/consts.rs
@@ -0,0 +1,3 @@
+use macroquad::{color::Color, color_u8};
+
+pub const BG_COLOR: Color = color_u8!(14, 14, 14, 255);
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use macroquad::prelude::*;
+mod consts;
+use consts::*;
+
+#[macroquad::main("plow")]
+async fn main() {
+    let mut camera_x = -screen_width() / 2.;
+    let mut camera_y = -screen_height() / 2.;
+    let mut camera_grid_size: f32 = 35.;
+    loop {
+        clear_background(BG_COLOR);
+        // handle input
+        if is_mouse_button_down(MouseButton::Left) {
+            let mouse_delta = mouse_delta_position();
+            camera_x += mouse_delta.x as f32 * screen_width() / 2.;
+            camera_y += mouse_delta.y as f32 * screen_height() / 2.;
+        }
+        let scroll = mouse_wheel();
+        if scroll.1 != 0. {
+            let mouse = mouse_position();
+            // i dont know how to actually do this scroll stuff
+            // but i made this curve in geogebra and i mean it looks cool
+            // i assumed it should grow exponentially, since it shant ever reach 0, only ever approach it as x decreases
+            let x = (mouse.0 + camera_x) / camera_grid_size;
+            let y = (mouse.1 + camera_y) / camera_grid_size;
+            let amt = 5_f32.powf(-0.001 * (scroll.1));
+            camera_grid_size /= amt;
+            // move camera position to zoom towards cursor
+            camera_x = x * camera_grid_size - mouse.0;
+            camera_y = y * camera_grid_size - mouse.1;
+        }
+        draw_rectangle(
+            5. * camera_grid_size - camera_x,
+            2. * camera_grid_size - camera_y,
+            camera_grid_size as f32,
+            camera_grid_size as f32,
+            GREEN,
+        );
+
+        next_frame().await
+    }
+}
